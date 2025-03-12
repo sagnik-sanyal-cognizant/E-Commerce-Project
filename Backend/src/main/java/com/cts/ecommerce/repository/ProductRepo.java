0b995df356b52bd99ce71050bb8c539cb6231e75
R <2389367@cognizant.com>
2025-03-12 13:51:04 +0530

    Added Product

---
+++ Backend/src/main/java/com/cts/ecommerce/repository/ProductRepo.java
@@ -0,0 +1,12 @@
+package com.cts.Ecommerce.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cts.Ecommerce.entity.Product;
+
+public interface ProductRepo extends JpaRepository<Product, Long> {
+    List<Product> findByCategoryId(Long categoryId);
+    List<Product> findByNameContainingOrDescriptionContaining(String name, String description);
+}
