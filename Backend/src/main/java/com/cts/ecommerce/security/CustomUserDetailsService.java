5877acef96890acb16ffa8fb38622a216f389f4f
Sanyal <2389376@cognizant.com>
2025-03-14 10:59:31 +0530

    Added Security

---
+++ Backend/src/main/java/com/cts/ecommerce/security/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.cts.ecommerce.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.cts.ecommerce.entity.User;
+import com.cts.ecommerce.exception.NotFoundException;
+import com.cts.ecommerce.repository.UserRepo;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepo userRepo;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepo.findByEmail(username)
+                .orElseThrow(()-> new NotFoundException("User/ Email Not found"));
+
+        return AuthUser.builder()
+                .user(user)
+                .build();
+    }
+}
