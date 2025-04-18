83282062e4ab7b0cefdfc1df207aa3cb25e1480b
Sanyal <2389376@cognizant.com>
2025-04-18 12:01:30 +0530

    admin order & order details

--- Frontend/src/app/admin/adminorder/adminorder.component.ts
+++ Frontend/src/app/admin/adminorder/adminorder.component.ts
@@ -22,14 +22,19 @@ export class AdminorderComponent implements OnInit {
   itemsPerPage: number = 10;
   error: any = null
 
+  // Array to hold possible order statuses
   OderStatus = ["PENDING", "CONFIRMED", "SHIPPED", "DELIVERED", "CANCELLED", "RETURNED"]
 
-  constructor(private apiService: ApiService, private router: Router) { }
+  constructor(
+    private apiService: ApiService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.fetchOrders();
   }
 
+  // Method to fetch orders from the API based on the search status
   fetchOrders(): void {
     const ordersObservable = this.searchStatus ? this.apiService.getAllOrderItemsByStatus(this.searchStatus)
       : this.apiService.getAllOrders();
@@ -44,6 +49,7 @@ export class AdminorderComponent implements OnInit {
     })
   }
 
+  // Method to set orders and update pagination details
   setOrders(orderList: any[]): void {
     this.orders = orderList
     this.totalPages = Math.ceil(orderList.length / this.itemsPerPage);
@@ -53,6 +59,7 @@ export class AdminorderComponent implements OnInit {
     )
   }
 
+  // Method to handle changes in the status filter
   handleFilterChange(): void {
     this.currentPage = 1;
     if (this.statusFilter) {
@@ -65,19 +72,21 @@ export class AdminorderComponent implements OnInit {
     }
   }
 
+  // Method to handle changes in the search status
   handleSearchStatusChanged():void{
     this.currentPage = 1;
     this.fetchOrders();
   }
 
+  // Method to handle page changes for pagination
   onPageChange(page: number):void{
     this.currentPage = page
     this.filterOrders = this.orders.slice((this.currentPage -1) * this.itemsPerPage, this.currentPage * this.itemsPerPage)
   }
 
+  // Method to navigate to the order details page
   navigateToOrderDetailsPage(id: number):void{
     this.router.navigate([`/admin/order-details/${id}`])
   }
-
 
 }
